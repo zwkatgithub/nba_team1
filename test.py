6473b8526d61201ecf5513a464c2ee5b84074834
xd-sonp <sonpking@163.com>
2018-03-13 17:09:49 +0800

    hello

--- test.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-html = '<div> hello</div> <!--<div>sonp</div>-->'
-soup = BeautifulSoup(html,'lxml')
-divs = soup.find_all('div')
-for div in divs:
-    print(div.text)
